b1b68357ae0e5267edb6cdee2d6d62e40ab4ccbe
Jen Rasmussen <jrasmussen@four51.com>
2016-02-25 14:13:28 -0600

    Custom Solutions Modules - adding backToTop, removing categoryDropdown

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var four51 = {};
-four51.app = angular.module('451order', ['ngResource', 'ngRoute', 'ngAnimate', 'ngSanitize', 'ngCookies', 'ngTouch', 'ui.validate', 'ui.mask', 'headroom', 'ui.bootstrap', 'angulartics',
-    'angulartics.google.analytics', 'ngAutocomplete', 'OrderCloud-AddToCartPreview', 'OrderCloud-Carousel', 'OrderCloud-CategoryCollapse', 'OrderCloud-LargeAddressListSearch',
-    'OrderCloud-Minicart', 'OrderCloud-ProductLightbox', 'compile-module', 'OrderCloud-ProductListAddToCart', 'OrderCloud-SecurityModal', 'OrderCloud-TermsModal', 'OrderCloud-ProductZoom',
-    'OrderCloud-SpecForms', 'OrderCloud-CategoryModal', 'OrderCloud-HamburgerNavigation', 'OrderCloud-CategoryDropdown', 'OrderCloud-FixedFooter']);
+four51.app = angular.module('451order', ['ngResource', 'ngRoute', 'ngAnimate', 'ngSanitize', 'ngCookies', 'ngTouch', 'ui.validate', 'ui.mask', 'headroom', 'ui.bootstrap',
+    'angulartics', 'angulartics.google.analytics', 'ngAutocomplete', 'OrderCloud-AddToCartPreview', 'OrderCloud-BackToTop', 'OrderCloud-Carousel', 'OrderCloud-CategoryCollapse',
+    'OrderCloud-LargeAddressListSearch', 'OrderCloud-Minicart', 'OrderCloud-ProductLightbox', 'compile-module', 'OrderCloud-ProductListAddToCart', 'OrderCloud-SecurityModal',
+    'OrderCloud-TermsModal', 'OrderCloud-ProductZoom', 'OrderCloud-SpecForms', 'OrderCloud-CategoryModal', 'OrderCloud-HamburgerNavigation', 'OrderCloud-FixedFooter']);
